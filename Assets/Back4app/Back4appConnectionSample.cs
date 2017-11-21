4326f9737941de2f38c6c5b66fb2bd464f0ee64e
dominic wong <dom@valoot.com>
2017-11-22 00:54:29 +0800

    Unity3d Project

---
+++ Assets/Back4app/Back4appConnectionSample.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+using System.Threading.Tasks;
+
+public class Back4appConnectionSample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ParseObject gameScore = new ParseObject ("GameSocre");
+		gameScore["score"] = 1337;
+		gameScore["playerName"] = "Sean Plott";
+		Task saveTask = gameScore.SaveAsync();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
